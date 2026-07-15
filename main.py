db9a41f325867a038f9e063e2d2a83b3cd4145bc
RAHU1027 <sanjitrai88888888@gmail.com>
2026-07-15 10:54:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,13 +6,16 @@ from telethon.sessions import StringSession
 import phonenumbers
 from phonenumbers import carrier, geocoder
 
-# Credentials
+# API Credentials
 API_ID = 21552435
 API_HASH = "5b108bd2fdd31c0c34bc65f24a5216a0"
+# Aapki string yahan seedhi rakhi hai taaki error na aaye
 SESSION_STRING = "BQFI3TMATHAm-_lYmmbFUW9xZSB4OY_cmhzahsyOwdzRnuoTq_ywbYsy6pTwxucMjmkR8V-x5r1CjqJfMPWgG19NTN2BUFr3v9e_68jjqIBm7_4kZj1GLd-Vt1R9y8KA6vFrUPzKWOHPNKCa-vJOuBCv_4F7Wm9m3tVm4CpkuG_M-58We5psd-qAaaTNxxm2VJ-qVTZ4JBkVCqS_QkqmYwH9z6uiW79e8GV2qeYSZK52Xhi-Idhf-XekKei7Lb7uplOTfiA7z-Nn2N0O91Jsd7Lt9wZcPxofrdwjMbLPzxPJ4vPRf8RiOYnoaM-1QRiyLw0kSNeJGqF8I0nXNeh53Gvf-Ys6OQAAAAHTR-i_AA"
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
+
+# Client Setup
 client = TelegramClient(StringSession(SESSION_STRING), API_ID, API_HASH)
 
 @app.on_event("startup")
@@ -37,7 +40,7 @@ async def search(request: Request, type: str = Form(...), query: str = Form(...)
                 "Phone": user.phone if user.phone else "Hidden"
             }
         except Exception:
-            result["error"] = "Telegram User not found."
+            result["error"] = "User nahi mila."
             
     elif type == "phone":
         try:
@@ -45,9 +48,9 @@ async def search(request: Request, type: str = Form(...), query: str = Form(...)
             result["data"] = {
                 "Location": geocoder.description_for_number(parsed, "en"),
                 "Operator": carrier.name_for_number(parsed, "en"),
-                "Valid": phonenumbers.is_valid_number(parsed)
+                "Valid": "Yes"
             }
         except Exception:
-            result["error"] = "Invalid Phone Number."
+            result["error"] = "Number invalid hai."
 
     return templates.TemplateResponse("index.html", {"request": request, "result": result})
